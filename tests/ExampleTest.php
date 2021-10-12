5b4190f8cf51502d90bb0b4fb5b7e8eea58ba0dc
ShibuyaKosuke <github@shibuyakosuke.com>
2021-10-13 01:44:33 +0900

    mod

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ShibuyaKosuke\LaravelJetAdminlte\Test;
+
+class ExampleTest extends TestCase
+{
+    public function testResolve()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ShibuyaKosuke\LaravelJetAdminlte\Test;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+
+}
