2ac9a5c521eac3b2cd8407457258c1f64097d0fe
ShibuyaKosuke <github@shibuyakosuke.com>
2021-10-13 02:02:11 +0900

    mod

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,6 +4,9 @@ namespace ShibuyaKosuke\LaravelJetAdminlte\Test;
 
 class ExampleTest extends TestCase
 {
+    /**
+     * @return void
+     */
     public function testResolve()
     {
         $this->assertTrue(true);
