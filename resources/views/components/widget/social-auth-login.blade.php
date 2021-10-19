340b63691d6748207f1b12023257922645c0ca25
ShibuyaKosuke <github@shibuyakosuke.com>
2021-10-20 03:21:58 +0900

    mod

--- resources/views/components/widget/social-auth-login.blade.php
+++ resources/views/components/widget/social-auth-login.blade.php
@@ -1,10 +1,12 @@
-<div class="social-auth-links text-center mt-2 mb-3">
-    @foreach (JetAdminLte::socialServices() as $social => $value)
-        @if($value)
-            <a href="{{ route('oauth', ['provider' => $social]) }}"
-               class="btn btn-block btn-social btn-{{ $social }}">
-                <i class="fab fa-{{ $social }} mr-2"></i> {{ __('jet-adminlte::adminlte.sign_in_using', ['provider' => ucfirst($social)]) }}
-            </a>
-        @endif
-    @endforeach
-</div>
+@if(JetAdminLte::hasSocialLoginFeature())
+    <div class="social-auth-links text-center mt-2 mb-3">
+        @foreach (JetAdminLte::socialServices() as $social => $value)
+            @if($value)
+                <a href="{{ route('oauth', ['provider' => $social]) }}"
+                   class="btn btn-block btn-social btn-{{ $social }}">
+                    <i class="fab fa-{{ $social }} mr-2"></i> {{ __('jet-adminlte::adminlte.sign_in_using', ['provider' => ucfirst($social)]) }}
+                </a>
+            @endif
+        @endforeach
+    </div>
+@endif
