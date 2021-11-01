1c1d5e4fac985a8dcf56112f23b7d84df31072fe
ShibuyaKosuke <github@shibuyakosuke.com>
2021-11-01 11:55:36 +0900

    mod

---
+++ resources/lang/en/columns.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'users' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'created_at' => 'Created at',
+        'updated_at' => 'Updated at',
+    ],
+];
