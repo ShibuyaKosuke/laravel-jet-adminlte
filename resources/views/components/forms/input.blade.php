637917c8f958b981dd4343144a0c1185d1da2587
ShibuyaKosuke <github@shibuyakosuke.com>
2021-12-03 01:45:34 +0900

    mod

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -7,6 +7,7 @@
            name="{{ $name }}"
            class="{{ $class ?? 'form-control' }} @error($name) is-invalid @enderror"
            value="{{ old($name, $value ?? null) }}"
+           @isset($placeholder) placeholder="{{ $placeholder }}" @endisset
     >
     @error($name)
     <span id="{{ $id ?? $name }}-error" class="error invalid-feedback">{{ $message }}</span>
