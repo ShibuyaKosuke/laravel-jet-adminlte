358f74fb8ff6089fb474c327158a7eb558df5eb5
ShibuyaKosuke <github@shibuyakosuke.com>
2021-12-03 23:31:41 +0900

    mod

--- src/Models/LinkedSocialAccount.php
+++ src/Models/LinkedSocialAccount.php
@@ -15,10 +15,22 @@ class LinkedSocialAccount extends Model
 {
     use HasFactory;
 
+    protected $table;
+
     /**
      * @var string[]
      */
     protected $fillable = ['provider_name', 'provider_id', 'avatar'];
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->table = config('jet_adminlte.social-account-table');
+
+        parent::__construct($attributes);
+    }
 
     /**
      * @return BelongsTo
