7714a45600224d19657df8394c9220dccef1e8c4
ShibuyaKosuke <github@shibuyakosuke.com>
2021-12-14 22:56:03 +0900

    WIP

--- resources/views/components/widget/sidebar-menu.blade.php
+++ resources/views/components/widget/sidebar-menu.blade.php
@@ -1,13 +1,15 @@
 <nav class="mt-2">
-    {!! $default->asUl(['class' => Arr::toCssClasses([
-        'nav',
-        'nav-pills',
-        'nav-sidebar',
-        'flex-column',
-        'nav-flat' => JetAdminLte::navFlatStyle(),
-        'nav-compact' => JetAdminLte::navCompact(),
-        'nav-child-indent' => JetAdminLte::navChildIndent(),
-        'nav-collapse-hide-child' => JetAdminLte::navChildHideOnCollapse(),
-        'nav-legacy' => JetAdminLte::navLegacyStyle(),
-    ]), 'role' => 'menu', 'data-widget' => 'treeview']) !!}
+    @isset($default)
+        {!! $default->asUl(['class' => Arr::toCssClasses([
+            'nav',
+            'nav-pills',
+            'nav-sidebar',
+            'flex-column',
+            'nav-flat' => JetAdminLte::navFlatStyle(),
+            'nav-compact' => JetAdminLte::navCompact(),
+            'nav-child-indent' => JetAdminLte::navChildIndent(),
+            'nav-collapse-hide-child' => JetAdminLte::navChildHideOnCollapse(),
+            'nav-legacy' => JetAdminLte::navLegacyStyle(),
+        ]), 'role' => 'menu', 'data-widget' => 'treeview']) !!}
+    @endisset
 </nav>
